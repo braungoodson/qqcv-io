8ca43e18ad242705587c79931f09c480527c2b9a
bragoods <bragoods@>
2016-04-29 18:48:06 -0400

    added purple aligator

---
+++ app/+purple-aligator/index.js
@@ -0,0 +1,1 @@
+System.register(["./purple-aligator.component"],function(exports_1,context_1){"use strict";var __moduleName=context_1&&context_1.id;return{setters:[function(purple_aligator_component_1_1){exports_1({PurpleAligatorComponent:purple_aligator_component_1_1["PurpleAligatorComponent"]})}],execute:function(){}}});
